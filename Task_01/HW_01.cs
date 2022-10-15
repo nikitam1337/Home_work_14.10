12e0d4ce5bb6de1313de4c8e460b55049991876c
nikitam1337 <nikitam.l@mail.ru>
2022-10-15 19:33:17 +0300

    Выполнил 1-ю задачу

---
+++ Task_01/HW_01.cs
@@ -0,0 +1,64 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+Console.Clear();
+
+int ArraySize = GetNumberFromUser("Введите число элементов массива: ", "Ошибка ввода!");
+int[] array = GetArray(ArraySize, 100, 999);
+int CountEven = GetEven(array);
+
+Console.Write($"Массив из {ArraySize} случайных положительных трёхзначных чисел: ");
+PrintArray(array);
+Console.WriteLine();
+Console.Write($"Количество чётных чисел в массиве = {CountEven}");
+
+////////// Определение методов///////////
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return res;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+
+int GetEven(int[] array)
+{
+    int count = 0;
+    foreach (int el in array)
+    {
+        if (el % 2 == 0) count++;
+    }
+    return count;
+}
+
+
+
