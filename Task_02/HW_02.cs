95dc064de31213f27d62524291aaf9eece0d7819
nikitam1337 <nikitam.l@mail.ru>
2022-10-15 19:54:20 +0300

    Выполнил 2-ю задачу

---
+++ Task_02/HW_02.cs
@@ -0,0 +1,60 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+Console.Clear();
+
+int ArraySize = GetNumberFromUser("Введите число элементов массива: ", "Ошибка ввода!");
+int[] array = GetArray(ArraySize, -100, 100);
+PrintArray(array);
+
+int SumEl = GetSumEl(array);
+Console.Write ($" -> Сумма элементов, стоящих на нечётных позициях = {SumEl}");
+
+////////// Определение методов///////////
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return res;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+
+int GetSumEl(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1) sum += array[i];
+    }
+    return sum;
+}
